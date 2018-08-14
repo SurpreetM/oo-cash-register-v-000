90e633b3fa475263e6db529ae5c87c895fab67df
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-14 19:56:30 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -13,7 +13,6 @@ class CashRegister
 
   def add_item(title, price, quantity = 1)
     self.total += price * quantity
-    
   end
 
   def apply_discount
@@ -28,6 +27,7 @@ class CashRegister
   end
 
   def items
+    
 
   end
 
